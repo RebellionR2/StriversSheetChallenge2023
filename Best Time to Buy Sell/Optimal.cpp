a911f3269aff135f9606b4eda2f46e78b8ee77d2
RebellionR2 <88272981+RebellionR2@users.noreply.github.com>
2023-06-06 21:33:54 +0530

    Create Optimal.cpp

---
+++ Best Time to Buy Sell/Optimal.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+
+   int maxProfit=INT_MIN;
+   int minPrice=INT_MAX;
+
+   for(int i=0; i<prices.size(); i++)
+   {
+       minPrice=min(minPrice,prices[i]);
+       maxProfit=max(maxProfit,prices[i]-minPrice);
+   } 
+
+   return maxProfit;
+   
+}
