a0190cdb9586108916d0de2f85f7649f8a1825b9
RebellionR2 <88272981+RebellionR2@users.noreply.github.com>
2023-06-06 18:25:08 +0530

    Create nextPermutationinbuild.cpp
     Here we have just used the in build method

---
+++ NextPermutation/nextPermutationinbuild.cpp
@@ -0,0 +1,12 @@
+//  Here we have just used the in build method 
+
+
+#include <bits/stdc++.h> 
+
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+
+next_permutation(permutation.begin(),permutation.end());
+return permutation;
+
+}
